b2d04810f67d4713ad665559980e9e0151ec0342
amogus3000 <bvalentin1415@gmail.com>
2023-06-19 14:47:36 +0300

    unique apps flyer

---
+++ app/src/main/java/com/miniclip/footb/ui/services/params/m_apps_flyer/repo/MyAppsFlyerRepo.kt
@@ -0,0 +1,11 @@
+package com.miniclip.footb.ui.services.params.m_apps_flyer.repo
+
+import com.appsflyer.AppsFlyerConversionListener
+import kotlin.coroutines.Continuation
+
+interface MyAppsFlyerRepo {
+    val myConversionListener: (Continuation<Map<String, String?>?>) -> AppsFlyerConversionListener
+
+    suspend fun getConversionMap(): Map<String, String?>?
+    fun getServiceUID(): String?
+}
