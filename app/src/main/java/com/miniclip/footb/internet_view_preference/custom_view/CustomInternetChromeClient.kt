6232310958535b4c859a130ca491fa6109c818e0
b.knabe <myse1lff@gmail.com>
2023-06-21 15:40:40 +0300

    Check links, fix bugs, passed over the checklist

--- app/src/main/java/com/miniclip/footb/internet_view_preference/custom_view/CustomInternetChromeClient.kt
+++ app/src/main/java/com/miniclip/footb/internet_view_preference/custom_view/CustomInternetChromeClient.kt
@@ -4,18 +4,24 @@ import android.net.Uri
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import io.github.boiawidmb9mb12095n21b50215b16132.b21nm01om5n1905mw0bdkb2b515.ObfustringThis
+import javax.inject.Inject
 
 /* Custom WebChromeClient */
-class CustomInternetChromeClient(
-    private val chromeCallback: (ValueCallback<Array<Uri>>?) -> Unit
-) : WebChromeClient() {
-    override fun onShowFileChooser(
-        view: WebView?,
-        mValueCallback: ValueCallback<Array<Uri>>?,
-        fileChooserParams: FileChooserParams?
-    ): Boolean {
-        chromeCallback(mValueCallback)
-        return true
+class CustomInternetChromeClient @Inject constructor() {
+    fun provideWebChromeClient(
+        view: WebView,
+        chromeCallback: (ValueCallback<Array<Uri>>?) -> Unit
+    ) {
+        view.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                view: WebView?,
+                valueCallback: ValueCallback<Array<Uri>>?,
+                fileParams: FileChooserParams?,
+            ): Boolean {
+                chromeCallback(valueCallback)
+                return true
+            }
+        }
     }
-
 }
