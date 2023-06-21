c5605bd43f4b8066c896cd3d865c568dd2ebec21
amogus3000 <bvalentin1415@gmail.com>
2023-06-21 16:56:22 +0300

    added pexel api

---
+++ app/src/main/java/com/miniclip/footb/services/image_api/DishApiImpl.kt
@@ -0,0 +1,29 @@
+package com.miniclip.footb.services.image_api
+
+import com.miniclip.footb.model.api.DishApiResponse
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class DishApiImpl @Inject constructor() {
+    /* TODO call to get photo*/
+    suspend fun requestDishPhoto(
+        userQuery: String
+    ): DishApiResponse? {
+        val dishPhotoResponse = pushQuery(userQuery)
+
+        return if (dishPhotoResponse?.body() != null && dishPhotoResponse.isSuccessful) {
+            dishPhotoResponse.body()
+        } else {
+            null
+        }
+    }
+
+    private suspend fun pushQuery(userQuery: String) = try {
+        ImageApiInstance.imageApiInstance?.getDishImage(userDish = userQuery)
+    } catch (e: HttpException) {
+        null
+    } catch (e: IOException) {
+        null
+    }
+}
